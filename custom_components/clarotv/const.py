1edffeccfc544c97ae98113b427cfe2a59ae4d83
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2022-04-24 22:41:58 -0300

    add const file

---
+++ custom_components/clarotv/const.py
@@ -0,0 +1,6 @@
+ATTRIBUTION = "Data provided by clarotv api"
+DOMAIN = "clarotv"
+CONF_CHANNEL_ID = "channel_id"
+
+BASE_URL = "https://programacao.claro.com.br/gatekeeper/exibicao/select?q=id_cidade:1&wt=json&sort=dh_inicio%20asc&fl=dh_inicio%20st_titulo%20titulo%20id_programa%20id_exibicao&fq=dh_inicio:%5B{}%20TO%20{}%5D&fq=id_canal:{}"
+NAME_LOGO_CHANNEL_URL = "https://programacao.claro.com.br/gatekeeper/canal/select?q=id_cidade:1&wt=json&rows=1&fq=id_canal:{}"
